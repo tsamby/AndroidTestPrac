6cf71d628c6239dca6004156be37d720c4a70f39
Novuyo Tsambani <noetsambani@gmail.com>
2018-01-29 14:25:07 +0200

    Third Commit

--- app/src/main/java/com/example/jason/memi/MainActivity.java
+++ app/src/main/java/com/example/jason/memi/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //memi app
 
     }
 
